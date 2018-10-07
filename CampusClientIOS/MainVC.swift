3ce4ff1038afde1f788a9dbacb486fe5af86112b
Денис Ефремов <mac@denyska.local>
2018-10-07 15:59:15 +0300

    Added tab bar, start to do "Поточне"

---
+++ CampusClientIOS/MainVC.swift
@@ -0,0 +1,23 @@
+//
+//  MainVC.swift
+//  CampusClientIOS
+//
+//  Created by mac on 07.10.2018.
+//  Copyright © 2018 SINED. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+       
+    
+
+}
