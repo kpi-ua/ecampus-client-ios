48ef9aa2efb4c8cd75fa42be6e36ad9cfe8bca90
Денис Ефремов <mac@denyska.local>
2018-10-08 20:05:11 +0300

    table view and nib file done

--- CampusClientIOS/Potochne/PotochneTVC.swift
+++ CampusClientIOS/Potochne/PotochneTVC.swift
@@ -10,43 +10,37 @@ import UIKit
 
 class PotochneTVC: UITableViewController {
     
-    let voteCell = VoteTVCell.init(style: UITableViewCellStyle.default, reuseIdentifier: "voteCell")
+    @IBOutlet var potocneTV: UITableView!
     
     let prepodi: [String] = ["prepod0", "prepod1", "prepod2", "prepod3", "prepod4"]
+    let cellIdentifier = "voteCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.title = "UITableView"
+        potocneTV.dataSource = self
+        potocneTV.delegate = self
+        
+        let nibName = UINib(nibName: "VoteTVCell", bundle: nil)
+        potocneTV.register(nibName, forCellReuseIdentifier: cellIdentifier)
     }
 
-    // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return prepodi.count
     }
-
-   /* override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        let cell = tableView.dequeueReusableCell(withIdentifier: "voteCell", for: indexPath)
-        let item = prepodi[indexPath.row]
-        return 0
-    }*/
-
-    /*override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //let cell = tableView.dequeueReusableCell(withIdentifier: "voteCell", for: indexPath)
-        //let item = prepodi[indexPath.row]
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = potocneTV.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! VoteTVCell
+        cell.nameOfPrepod(name: prepodi[indexPath.row])
         
-        let cell
-
         return cell
-    }*/
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
+
+
+
+
+    
     
 }
