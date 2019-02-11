e9de732deb21069484cb1d5a2f4ea40a2578867a
Денис Ефремов <mac@denyska.local>
2019-02-11 12:52:51 +0200

    working with profile

--- CampusClientIOS/Profile/ProfileTVC.swift
+++ CampusClientIOS/Profile/ProfileTVC.swift
@@ -10,33 +10,59 @@ import UIKit
 
 class ProfileTVC: UITableViewController {
 
-    let acountInfoReq = AccountInfo.init(apiClient: ApiClient.shared)
-    let sections = ["Персональні дані", "Дані за місцем навчання (роботи)", "Контакти"]
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var subdivisionLabel: UILabel!
+    @IBOutlet weak var groupLabel: UILabel!
+    @IBOutlet weak var marksLabel: UILabel!
+    @IBOutlet weak var studyFormLabel: UILabel!
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var speciltyLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var firstHeaderCell: UIView!
+    @IBOutlet weak var secondHeaderCell: UITableViewCell!
+    @IBOutlet weak var firstHeaderLabel: UILabel!
+    @IBOutlet weak var secondHeaderLabel: UILabel!
+    
+    let profileQ = DispatchQueue.global(qos: DispatchQoS.QoSClass.default)
+    let accountInfoReq = AccountInfo.init(apiClient: ApiClient.shared)
+    var info: AccountInfoS?
+    var groupInfo: [AcountGroup]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.title = "Мій профіль"
-        acountInfoReq.getAccountInfo { (info) in
-            print(info)
-        }
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return sections.count
+        firstHeaderCell.backgroundColor = themeColor
+        secondHeaderCell.backgroundColor = themeColor
+        firstHeaderLabel.textColor = UIColor.white
+        secondHeaderLabel.textColor = UIColor.white
+        makeRequests()
     }
     
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sections[section]
+    func makeRequests() {
+        profileQ.sync {
+            accountInfoReq.getAccountInfo { (info) in
+                self.info = info
+                self.setLabels()
+            }
+            accountInfoReq.getAccountGroup { (group) in
+                self.groupInfo = group
+                self.setLabels()
+            }
+        }
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    func setLabels() {
+        nameLabel.text = info?.name
+        subdivisionLabel.text = groupInfo?[0].cathedra
+        groupLabel.text = groupInfo?[0].studyGroupName
+        studyFormLabel.text = groupInfo?[0].studyformname
+        courseLabel.text = groupInfo?[0].studyCourse
+        speciltyLabel.text = groupInfo?[0].proftrain
+        tableView.reloadData()
     }
-
-   
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
